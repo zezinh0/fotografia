c2e2bd3573d56bcbecf176ae0c6f65f6bf13032a
José Costa <44607347+zezinh0@users.noreply.github.com>
2022-11-18 14:38:28 +0000

    Update Checkout.js

--- frontend/src/screen/Checkout.js
+++ frontend/src/screen/Checkout.js
@@ -95,7 +95,7 @@ export default function Checkout() {
     0
   );
   const stripePromise = loadStripe(
-    'pk_test_51LSXm2GgBxYtUt4K3h0nx3YO60JE2Pp6TxRoMnCuNj2UMYA0edBNzzq9W5V7YXDU9jJIjahPNhOaYHj3pdHDg5QY00w8HLH2aY'
+    ''
   );
   console.log(state);
   useEffect(() => {
